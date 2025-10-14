7e27e52602291b1888c5b7b8a772a40ef8f4b7a0
Oliwia Wądołowska <oliwia.wadolowska@gmail.com>
2025-10-14 15:47:36 +0200

    features 2-5

--- src/account.py
+++ src/account.py
@@ -1,5 +1,27 @@
 class Account:
-    def __init__(self, first_name, last_name):
+    def __init__(self, first_name, last_name, national_id, promo_code=None):
         self.first_name = first_name
         self.last_name = last_name
-        self.balance = 0
+        self.balance = 0.0
+        self.promo_code = promo_code
+        self.national_id = self.validate_national_id(national_id)
+        self.validate_promo()
+
+    def validate_national_id(self, national_id):
+        if len(national_id) == 11 and national_id.isdigit():
+            return national_id
+        return "Invalid"
+
+    @staticmethod
+    def get_birth_from_national_id(national_id):
+        yy = int(national_id[:2])
+        return 1900 + yy
+
+    def validate_promo(self):
+        if (
+            self.promo_code
+            and self.promo_code.startswith("PROM_")
+            and self.national_id != "Invalid"
+        ):
+            if self.get_birth_from_national_id(self.national_id) > 1960:
+                self.balance += 50.0
--- tests/unit/test_account_create.py
+++ tests/unit/test_account_create.py
@@ -3,9 +3,27 @@ from src.account import Account
 
 class TestAccount:
     def test_account_creation(self):
-        account = Account("John", "Doe")
+        account = Account("John", "Doe", "12345678901")
         assert account.first_name == "John"
         assert account.last_name == "Doe"
-    def test_account_start_balance(self):
-        account = Account("John", "Doe")
-        assert account.balance == 0
+        assert account.balance == 0.0
+        assert account.national_id == "12345678901"
+    def test_account_invalid_national_id(self):
+        too_long = Account("John", "Doe", "1234567890123")
+        too_short = Account("John", "Doe", "123")
+        invalid = Account("John", "Doe", "abc")
+        assert too_long.national_id == "Invalid"
+        assert too_short.national_id == "Invalid"
+        assert invalid.national_id == "Invalid"
+    def test_account_promo_code(self):
+        valid_code = Account("John", "Doe", "87110745612", "PROM_XYZ")
+        none_code = Account("John", "Doe", "12345678901")
+        wrong_code = Account("John", "Doe", "12345678901", "PROMOXYZ")
+        assert valid_code.balance == 50.0
+        assert none_code.balance == 0.0
+        assert wrong_code.balance == 0.0
+    def test_age_validation(self):
+        valid_age = Account("John", "Doe", "87110745612", "PROM_XYZ")
+        invalid_age = Account("John", "Doe", "55031412347", "PROM_XYZ")
+        assert valid_age.balance == 50.0
+        assert invalid_age.balance == 0.0
